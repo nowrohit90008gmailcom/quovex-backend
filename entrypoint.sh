9935be84c7384b613684ddde28a22e79dff5ccf6
Rohit <nowrohit90008@gmail.com>
2026-07-14 21:10:36 +0530

    Remove Docker-only files (native deployment)

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "entrypoint.sh: running alembic upgrade head..."
-alembic upgrade head
-
-echo "entrypoint.sh: starting uvicorn..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers "${UVICORN_WORKERS:-2}" --access-log
